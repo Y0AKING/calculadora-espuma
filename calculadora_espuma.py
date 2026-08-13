832c775ea1c782fa233bfb5528c781d461a025c4
Y0AKING <yoaking@gmail.com>
2026-08-12 21:57:38 -0500

    Update calculadora_espuma.py

--- calculadora_espuma.py
+++ calculadora_espuma.py
@@ -58,11 +58,11 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-st.title("🧯 Calculadora de Hidráulica")
+st.title("🧯 Calculadora de Hidráulica Aplicada")
 # Primera línea: Normal / Regular
 st.markdown("<p style='text-align: center; font-size: 14px; color: gray;'>Desarrollada por el Teniente Brigadier Joaquín Córdova Obal - Salvadora Ica N° 22 - VI Comandancia Departamental Ica</p>", unsafe_allow_html=True)
 # Segunda línea: En negrita / Bold
-st.markdown("<p style='text-align: center; font-weight: bold; font-size: 16px;'>Herramienta móvil de toma de decisiones para el cálculo de Aplicación de Espuma Contra Incendios</p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; font-weight: bold; font-size: 16px;'>Herramienta móvil de toma de decisiones para el cálculo de Aplicación en la lucha Contra Incendios</p>", unsafe_allow_html=True)
 
 st.divider()
 
